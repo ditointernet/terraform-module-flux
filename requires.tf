7aff68b2d2017350ef65de865496d466818b290d
Arthur Bailão <arthur.bailao@dito.com.br>
2023-10-20 16:45:45 -0300

    change kubectl lib

--- requires.tf
+++ requires.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1"
+      source  = "alekc/kubectl"
+      version = "2.0.3"
     }
 
     kustomization = {
